8508645536d1eaf0acfebd0d8a73134dcaa74127
XueFaming <faming.xue@hexagon.com>
2018-10-15 17:16:09 +0800

    Initial commit

---
+++ MvcApplication1008/Controllers/ApplicationController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class ApplicationController : Controller
+    {
+        //
+        // GET: /Application/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ApplicationView()
+        {
+            return View();
+        }
+
+    }
+}
